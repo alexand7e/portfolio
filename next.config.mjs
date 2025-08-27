6e4728bb615d186bc76bc07dbbd9beff08bbaa2b
alexand7e <alexandrepitstop@gmail.com>
2025-08-26 22:43:35 -0300

    stack e config do next com problemas, testando solução

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     images: {
         domains: ['alexand7e.dev.br'], // Replace with your allowed domains
     },
